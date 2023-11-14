a4ac2c9c3a5ac7a3d6adc05efc4f5977a8734958
Soroush <ranjbar.soroush32@gmail.com>
2023-11-14 09:43:54 +0330

    Header component designed and added to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Menu, About, Header, Footer } from "./components"
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Menu />}/>
+        <Route path="/About" element={<About />}/>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,20 @@
-
+import { Link } from "react-router-dom"
+import logo from '../Images/Logo.png'
 
 export const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <header className="flex justify-between px-16 items-center bg-black text-white">
+      <div className="perspective-1000">
+        <Link to={'/'}>
+          <img src={logo} className="w-32 h-32 rounded-md transform transition-transform ease-in-out hover:rotate-12"/>
+        </Link>
+      </div>
+      <div className="flex w-96 justify-around">
+        <Link to={'/'}>Menu</Link>
+        <Link to={'/about'}>About</Link>
+        <Link to={'/about'}>About</Link>
+        <Link to={'/about'}>About</Link>
+      </div>
+    </header>
   )
 }
