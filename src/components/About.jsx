8aecf1f27ad91444857a3dba203338ac5b7e65e9
Soroush <ranjbar.soroush32@gmail.com>
2023-11-14 10:11:09 +0330

    Menu and About is modified

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@
 export const About = () => {
   return (
     <div>
-      
+      About
     </div>
   )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
+export * from './Home';
+export * from './Contact';
 export * from './Menu';
 export * from './Header';
 export * from './Footer';
